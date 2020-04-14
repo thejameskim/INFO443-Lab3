e68c6577ed5e43097459dd05152ce0743c98540d
Nam Pham <npham24@Nams-MacBook-Pro.local>
2020-04-13 18:16:07 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # INFO443-Lab3
+
+1. Clone your forked repo into your local machine
+2. `cd` into the repo
+3. Create a development branch separate from master via `git checkout -b dev`
+4. Set up compiler configuration file tsconfig.json via `tsc --init`
+5. Enter command `npm install --save @types/node` to be able to use ES6 types in your function
+6. Code your design for the given specifications in problem-a.ts
+7. Test your changes by compiling the .ts file via `tsc ./exercises/twitter.ts`  or `tsc --watch ./exercises/twitter.ts` to auto-recompile on file save
+8. Read the exercise and try to refactor the given code so it has better style
+8. Run the script via `node ./exercise/twitter.js` and check if your code gives the intended result
+9. If your code passes the tests, `add`, `commit`, and `push` your code to `dev`.
+10. Create a pull request to merge your commits from `dev` into `master` in **your** repository, rather than the repo you forked from.
---
+++ exercises/twitter.ts
@@ -0,0 +1,96 @@
+/*
+    Design a simplified version of Twitter where users can post tweets, 
+    follow/unfollow another user and is able to see the 10 most recent tweets 
+    in the user's news feed. 
+    Your design should support the following methods:
+
+    postTweet(userId, tweetId): Compose a new tweet.
+    getNewsFeed(userId): Retrieve the 10 most recent tweet ids in the user's news feed. 
+    Each item in the news feed must be posted by users who the user followed or by the user herself. 
+    Tweets must be ordered from most recent to least recent.
+
+    follow(followerId, followeeId): Follower follows a followee.
+
+    unfollow(followerId, followeeId): Follower unfollows a followee.
+
+
+    Example:
+
+    twitter: Twitter = new Twitter();
+
+    // User 1 posts a new tweet (id = 5).
+    twitter.postTweet(1, 5);
+
+    // User 1's news feed should return a list with 1 tweet id -> [5].
+    twitter.getNewsFeed(1);
+
+    // User 1 follows user 2.
+    twitter.follow(1, 2);
+
+    // User 2 posts a new tweet (id = 6).
+    twitter.postTweet(2, 6);
+
+    // User 1's news feed should return a list with 2 tweet ids -> [6, 5].
+    // Tweet id 6 should precede tweet id 5 because it is posted after tweet id 5.
+    twitter.getNewsFeed(1);
+
+    // User 1 unfollows user 2.
+    twitter.unfollow(1, 2);
+
+    // User 1's news feed should return a list with 1 tweet id -> [5],
+    // since user 1 is no longer following user 2.
+    twitter.getNewsFeed(1);
+*/
+
+const NEWS_FEED_SIZE = 10;
+
+class Twitter {
+    
+    /** Initialize your data structure here. */
+    constructor() {
+        
+    }
+    
+    /** Compose a new tweet. */
+    public postTweet(userId: number, tweetId: number) {
+        
+    }
+    
+    /** Retrieve the NEWS_FEED_SIZE most recent tweet ids in the user's news feed. 
+        Each item in the news feed must be posted by users who the user followed or by the user herself. 
+        Tweets must be ordered from most recent to least recent. 
+    */
+    public getNewsFeed(userId: number): number[] {
+        return [];
+    }
+    
+    /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
+    public follow(followerId: number, followeeId: number) {
+        
+    }
+    
+    /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
+    public unfollow(followerId: number, followeeId: number) {
+        
+    }
+}
+
+// you can store your tweet in a linked-list-esque timeline
+class Tweet {
+    private data: string;
+    private next: Tweet | null;
+    constructor(private id: number) {
+        this.data = "";
+        this.next = null;
+    }
+}
+
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * let twitter: Twitter = new Twitter();
+ * twitter.postTweet(userId,tweetId);
+ * let newsfeed: number[] = twitter.getNewsFeed(userId);
+ * twitter.follow(followerId,followeeId);
+ * twitter.unfollow(followerId,followeeId);
+ */
