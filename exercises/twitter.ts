075fe2c9c4dc0cd4ba00c2b8a7ba4501341f7c22
Nam Pham <npham24@Nams-MacBook-Pro.local>
2020-04-13 18:16:59 -0700

    added problem source

--- exercises/twitter.ts
+++ exercises/twitter.ts
@@ -1,4 +1,5 @@
 /*
+    From: https://leetcode.com/problems/design-twitter/ 
     Design a simplified version of Twitter where users can post tweets, 
     follow/unfollow another user and is able to see the 10 most recent tweets 
     in the user's news feed. 
